158f50cf3e71c55ce1076d87c3fd3ba92d1c206f
farhan-haroon <theoriginalhybrid9@gmail.com>
2024-04-25 23:36:58 +0530

    remove extras

--- color_detection.py
+++ color_detection.py
@@ -30,7 +30,7 @@ while True:
     cv2.imshow('mask', mask)
     cv2.imshow('frame', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord(' '):
         break
 
 cap.release()
